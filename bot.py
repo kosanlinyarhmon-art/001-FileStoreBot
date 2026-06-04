a36619ded9da446a992a2c6d2ec3b8b8eed19821
kosanlinyarhmon-art <kosanlinyarhmon@gmail.com>
2026-06-04 15:27:36 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,40 +1,33 @@
 # -*- coding: utf-8 -*-
-import os
 import logging
-import logging.config
 from flask import Flask
 from threading import Thread
+from pyromod import listen
+from pyrogram import Client
+from config import *
 
+# Flask setup
 app = Flask(__name__)
 @app.route('/')
 def home():
     return "Bot is running!"
 
-def run():
+def run_flask():
     app.run(host='0.0.0.0', port=10000)
 
-# Bot မ run ခင် ဒီ thread ကို အရင် run ပါ
-Thread(target=run).start()
-
-# Get logging configurations
-logging.getLogger().setLevel(logging.ERROR)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-from pyromod import listen
-from pyrogram import Client
-from config import *
-
-def main():
-    plugins = dict(root="plugins")
-    app = Client("FileStore",
-                 bot_token=BOT_TOKEN,
-                 api_id=API_ID,
-                 api_hash=API_HASH,
-                 plugins=plugins,
-                 workers=100)
-
-    app.run()
-
+# Bot setup
+plugins = dict(root="plugins")
+bot = Client("FileStore",
+             bot_token=BOT_TOKEN,
+             api_id=API_ID,
+             api_hash=API_HASH,
+             plugins=plugins,
+             workers=100)
 
 if __name__ == "__main__":
-    main()
+    # Flask ကို Thread နဲ့ run
+    Thread(target=run_flask).start()
+    
+    # Bot ကို run
+    print("Bot is starting...")
+    bot.run()
